fd84009340b9dd8403a33d4be5de672e7fc9764c
jvdkwast <h.vanderkwast@un-ihe.org>
2019-02-18 02:26:26 +0100

    walk backward

--- walk.py
+++ walk.py
@@ -4,14 +4,14 @@ import turtle
 t = turtle.Turtle()
 
 # set colors
-colors = ['red', 'blue']
-
+colors = ['red','blue']
+t.begin_fill()
 for j in range(8):
+
     t.color(colors[j % 2])
     for i in range(18):
-        t.forward(20)
-        t.left(20)
-    t.right(45)
-
-
+        t.backward(20)
+        t.right(20)
+    t.left(45)
+t.end_fill()
 turtle.mainloop()
