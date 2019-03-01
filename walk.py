38a3cb090ec08033f08d3cdcebdd04ddb10fe468
CMicha <claire.michailovsky@gmail.com>
2019-03-01 15:22:21 +0100

    turtle turtle

--- walk.py
+++ walk.py
@@ -4,14 +4,18 @@ import turtle
 t = turtle.Turtle()
 
 # set colors
-colors = ['red', 'blue']
+colors = ['green']
+t.color(colors[0])
+t.left(90)   
+for i in range(9):
+    t.forward(25)
+    t.left(20)
+t.forward(25)
+t.left(90)   
+t.forward(180)   
 
-for j in range(8):
-    t.color(colors[j % 2])
-    for i in range(18):
-        t.forward(20)
-        t.left(20)
-    t.right(45)
-
-
+t.left(90)   
+for i in range(9):
+    t.forward(7)
+    t.left(20)
 turtle.mainloop()
