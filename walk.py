997bfe369c09c10bd122ef20ebceab65e335d8c3
Raymond Nijssen <r.nijssen@terglobo.nl>
2019-03-01 12:31:07 +0100

    smaller leaves

--- walk.py
+++ walk.py
@@ -3,9 +3,9 @@ import turtle
 t = turtle.Turtle()
 
 for j in range(8):
-    for i in range(36):
+    for i in range(18):
         t.forward(20)
-        t.left(10)
+        t.left(20)
     t.right(45)
 
 turtle.mainloop()
