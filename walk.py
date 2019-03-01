72bd2ce2024d9c527b9edef56218dd6d9966d0d3
Raymond Nijssen <r.nijssen@terglobo.nl>
2019-03-01 12:27:16 +0100

    color

--- walk.py
+++ walk.py
@@ -2,10 +2,14 @@ import turtle
 
 t = turtle.Turtle()
 
+colors = ['red', 'blue']
+
 for j in range(8):
+    t.color(colors[j % 2])
     for i in range(36):
         t.forward(20)
         t.left(10)
     t.right(45)
+
 
 turtle.mainloop()
