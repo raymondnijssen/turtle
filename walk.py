c8b0442b4df7a4a7ca14ec949c1dab56ac65e302
Raymond Nijssen <r.nijssen@terglobo.nl>
2019-03-01 15:27:03 +0100

    larger leaves

--- walk.py
+++ walk.py
@@ -11,7 +11,7 @@ colors = ['red', 'blue']
 for j in range(8):
     t.color(colors[j % 2])
     for i in range(18):
-        t.forward(20)
+        t.forward(25)
         t.left(20)
     t.right(45)
 
