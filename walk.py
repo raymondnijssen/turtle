523329e59a71a1a3946484b0c314977c6ae9d43f
Raymond Nijssen <r.nijssen@terglobo.nl>
2019-03-01 15:51:32 +0100

    yellow leaves closes #1

--- walk.py
+++ walk.py
@@ -9,7 +9,7 @@ t.shapesize(5, 5, 5)
 t.begin_fill()
 t.pensize(10)
 # set colors
-colors = ['red','blue']
+colors = ['red','yellow']
 t.begin_fill()
 for j in range(8):
 
@@ -21,15 +21,15 @@ for j in range(8):
 t.end_fill()
 colors = ['green']
 t.color(colors[0])
-t.left(90)   
+t.left(90)
 for i in range(9):
     t.forward(25)
     t.left(20)
 t.forward(25)
-t.left(90)   
-t.forward(180)   
+t.left(90)
+t.forward(180)
 
-t.left(90)   
+t.left(90)
 for i in range(9):
     t.forward(7)
     t.left(20)
