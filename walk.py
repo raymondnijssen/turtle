65e6529efab15f18056ea1e671c4cd2c86f15fd6
Raymond Nijssen <r.nijssen@terglobo.nl>
2019-03-01 12:21:08 +0100

    flower

--- walk.py
+++ walk.py
@@ -2,8 +2,10 @@ import turtle
 
 t = turtle.Turtle()
 
-for i in range(36):
-    t.forward(20)
-    t.left(10)
+for j in range(8):
+    for i in range(36):
+        t.forward(20)
+        t.left(10)
+    t.right(45)
 
 turtle.mainloop()
